3f0bc414b306dff1e9da88da4ac4d51c8c8a21e9
joshuawootonn <joshuawootonn@gmail.com>
2019-05-28 23:33:18 -0500

    basic flow of the pages setup

---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const About = () => (
+  <Layout>
+    <SEO title="About" />
+    <h1>About</h1>
+    <Link to="/">{'<- Back'}</Link>
+  </Layout>
+)
+
+export default About
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,14 +7,13 @@ import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <SEO title="Work" />
+    <h1>Work</h1>    
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Link to="/about/">About</Link>
+    <Link to="/contact/">Contact</Link>
   </Layout>
 )
 
