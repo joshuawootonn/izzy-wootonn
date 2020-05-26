ada1772ad538c87f0b8c7ad992dc39086d8726ca
joshuawootonn <joshuawootonn@gmail.com>
2020-05-26 11:44:50 -0500

    feat: header

--- src/components/header.component.js
+++ src/components/header.component.js
@@ -1,48 +1,44 @@
 import React from 'react';
-import { graphql, Link, useStaticQuery } from 'gatsby';
-import Img from 'gatsby-image';
+import { Link } from 'gatsby';
 import { css } from 'styled-components/macro';
 import dimensions from '../constants/dimensions';
 import zIndex from '../constants/zIndex';
+import Logo from '../images/logo-bw.svg';
 
 const styles = {
     root: css`
         height: ${dimensions.header}px;
         display: flex;
         flex-direction: row;
+        align-items: center;
         justify-content: space-between;
         position: fixed;
         z-index: ${zIndex.header};
         width: 100vw;
+        padding: 0 60px 0 30px;
+    `,
+    logo: css`
+        width: 155px;
+        height: auto;
+    `,
+    linkBox: css`
+        display: flex;
+        flex-direction: row;
+        a {
+            text-decoration: none;
+            color: black;
+            margin-left: 20px;
+        }
     `,
 };
 
-const query = graphql`
-    {
-        file(relativePath: { eq: "thumbnail.png" }) {
-            childImageSharp {
-                fluid {
-                    ...GatsbyImageSharpFluid_withWebp
-                }
-            }
-        }
-    }
-`;
-
 const HeaderComponent = () => {
-    const data = useStaticQuery(query);
     return (
         <div css={styles.root}>
-            <Link to="/">
-                <Img
-                    css={css`
-                        width: 70px;
-                        header: auto;
-                    `}
-                    fluid={data.file.childImageSharp.fluid}
-                />
+            <Link to="/film">
+                <img css={styles.logo} src={Logo} />
             </Link>
-            <div>
+            <div css={styles.linkBox}>
                 <Link to="/film">Film</Link>
                 <Link to="/about">About</Link>
             </div>
